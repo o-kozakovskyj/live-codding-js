8e580e6d3b82a2f87813070fdcf0c0b804ec4311
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-06-29 15:49:37 +0300

    prettier setup

--- functions/task1/index.js
+++ functions/task1/index.js
@@ -6,20 +6,20 @@ const res = getSenseOfLife();
 console.log(res);
 console.log(getSenseOfLife());
 
-const getSquared = (num) => {
+const getSquared = num => {
   return num * num;
-}
+};
 // ---test data
 console.log(getSquared(3));
- 
-const sum = ((numOne, numTwo) => numOne + numTwo);
+
+const sum = (numOne, numTwo) => numOne + numTwo;
 // ---test data---
 console.log(sum(3, 16));
 console.log(sum(-4, 5));
 console.log(sum(0, -1));
 console.log(sum('5', -1));
 
-const printMessage = ((age) => `I am ${age} years old`);
+const printMessage = age => `I am ${age} years old`;
 //  ---test data---
 console.log(printMessage(34));
 console.log(printMessage('34'));
@@ -32,9 +32,9 @@ function sumFunc(from, to) {
   }
   return res;
 }
-console.log(sumFunc(5, 8))
+console.log(sumFunc(5, 8));
 
-const mult = ((firstNum, secondNum) => firstNum * secondNum);
+const mult = (firstNum, secondNum) => firstNum * secondNum;
 //  ---test data---
 console.log(mult(3, 16));
 console.log(mult(3, -10e20));
@@ -42,7 +42,7 @@ console.log(mult(100e10, 16));
 console.log(mult(0, 16));
 console.log(mult(3, -16));
 
-const getsquaredArrow = (num => num * num);
+const getsquaredArrow = num => num * num;
 //  ---test data---
 console.log(getsquaredArrow(6));
 console.log(getsquaredArrow(0));
@@ -54,7 +54,6 @@ const getMagicNumber = () => 17;
 console.log(getMagicNumber());
 console.log(getMagicNumber([]));
 console.log(getMagicNumber(100));
-
 
 function sumFunc(from, to) {
   let result = 0;
